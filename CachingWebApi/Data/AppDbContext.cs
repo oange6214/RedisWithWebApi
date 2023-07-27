027861ef0e03fa43100431582fe019606973467b
Jed <oange6214@gmail.com>
2023-07-27 21:46:58 +0800

    Add project files.

---
+++ CachingWebApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CachingWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CachingWebApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Driver> Drivers { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : 
+        base(options)
+    {
+    }
+}
---
+++ CachingWebApi/Models/Driver.cs
@@ -0,0 +1,8 @@
+﻿namespace CachingWebApi.Models;
+
+public class Driver
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DriveNb { get; set; }
+}
